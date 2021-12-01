9e6487da7fa9152dcdc03fee0b80a227b4a49733
hugo.cabral <hugocabral2803@gmail.com>
2021-12-01 19:23:12 -0300

    feat Repository RoomRepository

---
+++ src/main/java/com/gerenciador/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.gerenciador.saladereuniao.repository;
+
+import com.gerenciador.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
